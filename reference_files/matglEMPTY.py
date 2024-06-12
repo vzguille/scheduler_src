5d60721d01b70c419c2cd42e6e04a10423218f3d
vtguille <vazqueztguillermo@gmail.com>
2024-06-12 10:04:51 -0500

    added reference files

---
+++ reference_files/matglEMPTY.py
@@ -0,0 +1,79 @@
+import pickle
+from matgl.models import M3GNet
+from matgl.ext.pymatgen import Structure2Graph, get_element_list
+from matgl.utils.training import PotentialLightningModule
+from matgl.graph.data import MGLDataset, MGLDataLoader, collate_fn_pes
+
+from dgl.data.utils import Subset
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
+
+
+with open('../all_static_labels.pkl', 'rb') as f:
+    all_static_labels = pickle.load(f)
+with open('../all_static_structures.pkl', 'rb') as f:
+    all_static_structures = pickle.load(f)
+with open('../train_static_index.pkl', 'rb') as f:
+    train_static_index = pickle.load(f)
+with open('../test_static_index.pkl', 'rb') as f:
+    test_static_index = pickle.load(f)
+with open('../val_static_index.pkl', 'rb') as f:
+    val_static_index = pickle.load(f)
+
+
+element_types = get_element_list(all_static_structures)
+
+converter = Structure2Graph(element_types=element_types, cutoff=5.0)
+dataset = MGLDataset(
+    threebody_cutoff=4.0,
+    structures=all_static_structures,
+    converter=converter,
+    labels=all_static_labels,
+)
+train_data = Subset(
+    dataset,
+    train_static_index
+)
+val_data = Subset(
+    dataset,
+    val_static_index
+)
+test_data = Subset(
+    dataset,
+    test_static_index
+)
+
+train_loader, val_loader, test_loader = MGLDataLoader(
+    train_data=train_data,
+    val_data=val_data,
+    test_data=test_data,
+    collate_fn=collate_fn_pes,
+    batch_size!VAR,
+    num_workers!VAR,
+)
+
+model = M3GNet(
+    element_types=element_types,
+    is_intensive=False,
+)  # potential model
+
+lit_module = PotentialLightningModule(model=model, 
+                                      energy_weight!VAR, 
+                                      force_weight!VAR, 
+                                      stress_weight!VAR,)
+
+logger = CSVLogger("logs", 
+                   name!VAR
+                   )
+
+trainer = pl.Trainer(max_epochs!VAR,
+                     logger=logger,
+                     inference_mode=False,
+                     )  # initializing trainer
+
+trainer.fit(model=lit_module, train_dataloaders=train_loader, 
+                val_dataloaders=val_loader) # we use the module and the initialized trainer
+
+model_export_path = "./model/"
+model.save(model_export_path)
